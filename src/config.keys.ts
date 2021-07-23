4aa08c4510acf8fe6da650b28802db5465b67021
akwganiu <lmd4sure@gmail.com>
2021-07-23 08:48:08 +0100

    keys abstracted

--- src/config.keys.ts
+++ src/config.keys.ts
@@ -1,8 +1,8 @@
 export default {
     DBURI: 'mongodb://localhost/sfsDb',
     BASEURL: 'http://localhost:3000',
-    HOST: 'bywayofdigital.com',
-    USERNAME: 'reply@bywayofdigital.com',
-    PASS: 'lEy-jwrL6BqiF1zv$a',
-    JWT_SECRET: 'fdferuoeofjsdfhfuweorerwnfdlfjieurue8794393ndhenfsdfj'
+    HOST: 'XXXXXXXXXXXXXXXXXXX',
+    USERNAME: 'XXXXXXXXXXXXXXXX',
+    PASS: 'XXXXXXXXXXXXXXX',
+    JWT_SECRET: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 }
